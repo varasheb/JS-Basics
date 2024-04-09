6f4f530f03d718e57a8f71e054072f4faa455a48
varasheb <varkanthi456@gmail.com>
2024-04-09 10:57:19 +0530

    Npm

---
+++ javaScript/calci.js
@@ -0,0 +1,14 @@
+console.log("Hello World")
+
+exports.add=function add(a,b){
+    return a+b
+}
+exports.sub=function sub(a,b){
+    return a-b
+}
+exports.mul=function mul(a,b){
+    return a*b
+}
+exports.div=function div(a,b){
+    return a/b
+}
---
+++ javaScript/npm.js
@@ -0,0 +1,52 @@
+//! npm
+
+// var calci=require("./calci")
+
+// console.log(calci.add(4,5))
+// console.log(calci.sub(4,5))
+// console.log(calci.mul(4,5))
+// console.log(calci.div(4,5))  
+
+
+//! Read a File
+// const fs = require('fs');
+// const { send } = require("./calculator.mjs")
+// fs.readFile('javaScript/calci.js', 'utf8', (err, data) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     console.log(data);
+// })
+
+//! write a file and remove the content
+// const fs2 = require('fs');
+
+// fs2.writeFile('javaScript/calci.js', 'console.log("Hello World")', (err) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     console.log('File written successfully');
+// })
+//! append file
+// const fs=require('fs')
+// fs.appendFile('javaScript/calci.js', 'console.log("Hello World")', (err) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     console.log('File written successfully');
+// })
+//! delete file
+// const fs=require('fs')
+// fs.unlink('javaScript/calci.js', (err) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     console.log('File deleted successfully');
+// })
+
+
+
