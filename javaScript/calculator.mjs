dc25156767543ce48577ddd403b1b57be6109f33
varasheb <varkanthi456@gmail.com>
2024-04-10 20:09:07 +0530

    [Varshab] Modified . folder structure

--- javaScript/calculator.mjs
+++ javaScript/calculator.mjs
@@ -1,16 +1,16 @@
-// export function add(a,b){
-//     return a+b
-// }
-// export function sub(a,b){
-//     return a-b
-// }
-// export function mul(a,b){
-//     return a*b
-// }
-// export function div(a,b){
-//     return a/b
-// }
-//! export 
+export function add(a,b){
+    return a+b
+}
+export function sub(a,b){
+    return a-b
+}
+export function mul(a,b){
+    return a*b
+}
+export function div(a,b){
+    return a/b
+}
+
 export class send{
      add(a,b){
         return a+b
