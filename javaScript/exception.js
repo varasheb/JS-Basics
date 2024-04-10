b0ee1b5ae0d14823e83bbf97d790456a554a6bae
varasheb <varkanthi456@gmail.com>
2024-04-10 20:06:13 +0530

    [Varshab] Modified . exception

--- javaScript/exception.js
+++ javaScript/exception.js
@@ -11,50 +11,50 @@ function divide(a, b) {
 
 
 
-// function divide(a, b) {
-//     try {
-//       if (b === 0) {
-//         throw new Error('Division by zero is not allowed.');
-//       }
-//       return a / b;
-//     } catch (error) {
-//       console.error('An error occurred:', error.message);
-//     } finally {
-//       console.log('Division operation complete.');
-//     }
-//   }
+function divide(a, b) {
+    try {
+      if (b === 0) {
+        throw new Error('Division by zero is not allowed.');
+      }
+      return a / b;
+    } catch (error) {
+      console.error('An error occurred:', error.message);
+    } finally {
+      console.log('Division operation complete.');
+    }
+  }
   
-//   console.log(divide(10, 2)); 
+  console.log(divide(10, 2)); 
   console.log(divide(10, 0)); 
   
   //! working with json parsing errors
-//   const invalidJsonString = '{"name": "John", "age": }';
-//     try {
-//         const parsedObject = JSON.parse(invalidJsonString);
-//         console.log(parsedObject);
-//     } catch (error) {
-//         console.error('Error parsing JSON:', error.message);
-//     }
+  const invalidJsonString = '{"name": "John", "age": }';
+    try {
+        const parsedObject = JSON.parse(invalidJsonString);
+        console.log(parsedObject);
+    } catch (error) {
+        console.error('Error parsing JSON:', error.message);
+    }
     
 //!Accessing properties of potentially undefined objects
-// const user = {};
+const user = {};
 
-// try {
-//   console.log(user.address.city); 
-// } catch (error) {
-//   console.error('Error accessing property:', error.message);
-// }
+try {
+  console.log(user.address.city); 
+} catch (error) {
+  console.error('Error accessing property:', error.message);
+}
 
   
 //! custom exception
-// function validateInput(input) {
-//     if (input === null || input === undefined || input === '') {
-//       throw new Error('Input cannot be empty');
-//     }
-//   }
+function validateInput(input) {
+    if (input === null || input === undefined || input === '') {
+      throw new Error('Input cannot be empty');
+    }
+  }
   
-//   try {
-//     validateInput('');
-//   } catch (error) {
-//     console.error('Validation error:', error.message);
-//   }
+  try {
+    validateInput('');
+  } catch (error) {
+    console.error('Validation error:', error.message);
+  }
