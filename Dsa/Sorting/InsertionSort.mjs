f7e2651dffb3d8ba8b4688a65d04c51625bd094a
varasheb <varkanthi456@gmail.com>
2024-04-17 12:03:03 +0530

    [Varshab] Add . Insertion sort

---
+++ Dsa/Sorting/InsertionSort.mjs
@@ -0,0 +1,18 @@
+//! Insertion sort
+
+export function insertionSort(arr) {
+    let n = arr.length;
+    for (let i = 1; i < n; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
+
+let arr = [5,4,3,23,24];
+console.log("Insertion Sort :",insertionSort(arr))
